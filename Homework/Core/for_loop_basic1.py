40e48f8ca322017897f568172c7383e152a72252
ShalynnBurger <shalynnburger@gmail.com>
2022-08-29 17:38:36 -0500

    for_loop_basic1

---
+++ Homework/Core/for_loop_basic1.py
@@ -0,0 +1,41 @@
+#BASIC
+
+# for i in range(151):
+#     print(i)
+
+#MULTIPLES-OF-FIVE
+
+# for i in range(5, 1001, 5):
+#     print(i)
+
+#COUNTING_THE-DOJO-WAY
+
+# for i in range(1, 101, 1):
+#     if i%10==0:
+#         print("Coding Dojo")
+#     elif i%5==0:
+#         print("Coding")
+#     else:
+#         print(i)
+
+#WOAH-THAT-SUCKER-HUGE
+# sum=0
+# for i in range(1, 500001, 2):
+#     sum+=i
+#     #sum = sum+i
+# print(sum)
+
+#COUNTDOWN-BY-FOURS
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+#FLEXIBLE-COUNTER
+
+# lowNum=2
+# highNum=35
+# mult=4 
+
+# for i in range(2, 35, 1):
+#     if i%4==0:
+#         print(i)
