f847917e1468d47cc961744254f1281650e26b77
ShalynnBurger <shalynnburger@gmail.com>
2022-08-30 20:11:21 -0500

    nested_dictionaries

--- Homework/Core/Nested_dictionaries&Lists/nested.py
+++ Homework/Core/Nested_dictionaries&Lists/nested.py
@@ -54,4 +54,9 @@ dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-# printInfo(dojo)
+
+def printInfo(dojo):
+    for item in dojo():
+        print(f'{len('locations')} {key.upper()}')
+        for i in range(0,len(val)):
+            print(val[i])
