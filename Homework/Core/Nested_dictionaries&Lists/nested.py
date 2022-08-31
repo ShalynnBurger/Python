5c632023f54b850a4f22766ed2f325988c27c4f4
ShalynnBurger <shalynnburger@gmail.com>
2022-08-30 20:11:10 -0500

    nested_dictionaries

---
+++ Homework/Core/Nested_dictionaries&Lists/nested.py
@@ -0,0 +1,57 @@
+# print("sanity check")
+
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+
+# x[1][0] = 15 
+# print(x)
+
+# print("=================================")
+
+# students[0]['last_name'] = 'Bryant'
+# print(students[0])
+
+# print("=================================")
+
+# sports_directory['soccer'][0] = 'Andres'
+# print(sports_directory['soccer'])
+
+# print("=================================")
+
+# z[0]['y'] = 30
+# print(z)
+
+# print("=================================")
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# for name in students:
+#     print("first_name - " +name['first_name'], "last_name - " + name['last_name'])
+
+# print("=================================")
+
+# for name in students:
+#     print(name['first_name'])
+#     print(name['last_name'])
+
+
+print("=================================")
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+# printInfo(dojo)
