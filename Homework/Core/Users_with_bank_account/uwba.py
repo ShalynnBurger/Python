5bda320b26ef72e57403e75add4c21fbc4dcc99b
ShalynnBurger <shalynnburger@gmail.com>
2022-09-02 12:38:14 -0500

    users_with_bank_accounts

---
+++ Homework/Core/Users_with_bank_account/uwba.py
@@ -0,0 +1,56 @@
+# print("sanity check")
+
+class BankAccount:
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposite(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(self.balance)
+        return self
+
+    def yield_interest(self):
+        self.balance += (self.balance * self.int_rate)
+        return self
+
+class User: 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def make_deposit(self, amount):
+        self.account.deposite(amount)
+        return self
+
+    def make_withdraw(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_user_balance(self):
+        self.account.display_account_info()
+        return self
+
+
+shay = User('Shalynn', 'snburger@gmail.com')
+
+shay.make_deposit(200).make_withdraw(30).display_user_balance()
+
+
+
+# shay = BankAccount(0.17, 500)
+
+# maverick = BankAccount(0.25, 750)
+
+
+# shay.deposite(90).deposite(45).deposite(350).withdraw(150).yield_interest().display_account_info()
+
+# maverick.deposite(120).deposite(500).withdraw(10).withdraw(200).withdraw(70).withdraw(50).yield_interest().display_account_info()
