d86e6df31ecb45dcc9c4670b38ba8a79abbe248a
ShalynnBurger <shalynnburger@gmail.com>
2022-09-03 02:53:07 -0500

    intro_into_flask

--- Intro_to_flask/intro_flask.py
+++ Intro_to_flask/intro_flask.py
@@ -1,10 +1,32 @@
 # print('sanity check')
 
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+
+
+
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return 'we can change this!'  # Return the string 'Hello World!' as a response
+
+@app.route('/success')
+def success():
+    return "success"
+
+
+@app.route('/say/<word>')
+def say_word(word):
+    return f"the word you gave me is: {word}"
+
+@app.route("/template")
+def template():
+    return render_template("index.html")
+
+
+#AWAYS AT THE BOTTOM
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
+
+
 
